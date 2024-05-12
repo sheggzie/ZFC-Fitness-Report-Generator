9fc1043073d71fff7a5a0de774928e016759c630
sheggzie <54886002+quadrey-sheggzie@users.noreply.github.com>
2024-05-12 16:39:34 +0000

    ...

--- zeal fitness club report generator/main/main_y.py
+++ zeal fitness club report generator/main/main_y.py
@@ -1,29 +1,33 @@
 from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 
-sheet = pd.read_excel('april.xlsx', sheet_name='Actually')
+xls = pd.ExcelFile('specimen.xlsx')
 
-text = sheet.iloc[31, [4, 5, 6]]
+df = xls.sheet_names
 
-# Load the font you want to use
-font = ImageFont.load_default()
+for i in range(len(df)): 
+    sheet = pd.read_excel(xls, sheet_name=df[i])
+    text = sheet.iloc[31, 4:]
+    
+    # Load the font you want to use
+    font = ImageFont.load_default()
 
-# Create an empty image
-img = Image.new('RGB', (600, 500), color = (255, 255, 255))
+    # Create an empty image
+    img = Image.new('RGB', (600, 500), color = (255, 255, 255))
 
-# Initialize ImageDraw object
-draw = ImageDraw.Draw(img)
+    # Initialize ImageDraw object
+    draw = ImageDraw.Draw(img)
+    
+    # Set initial position for text drawing
+    x = 10
+    y = 10
 
-# Set initial position for text drawing
-x = 10
-y = 10
+    draw.text((x, y), text.to_string(), fill=(0, 0, 0), font=font)
 
-draw.text((x, y), text.to_string(), fill=(0, 0, 0), font=font)
+    # Save the image
+    img.save(df[i]+'.png')
 
-# Save the image
-img.save('output_image_new.png')
-
-print("Image created successfully!")
+    print("Image created successfully!")  
 
 
 
